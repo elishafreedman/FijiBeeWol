e9cafbe7dcebcf5e2d6f5872db843769f613ef9e
Elisha Freedman <freedmanelisha@gmail.com>
2022-09-29 19:05:25 +1000

    updated script

--- test.R
+++ test.R
@@ -8,4 +8,17 @@ N<-ceiling(runif(n=8,min=1,max=20))
 
 N_sample <- runif(8, min= 2, max = 50)
 
-test_tree <- star_tips(tree = woltree, clade.label = clade.label, nodes = nodes , tip.label = tip.label)
+test_tree <- Backbone(tree = tree, clade.label = clade.label, nodes = N , tip.label = tip.label)
+
+
+#manually setting depth
+depths <- c(0.5*woltree$edge.length[170], 0.5*woltree$edge.length[149], 0.5*woltree$edge.length[143], 0.5*woltree$edge.length[135])
+newtree <- phylo.toBackbone(woltree, trans = trans)
+
+setEPS()
+postscript("woltree.eps", width = 50, height = 100)
+plot(woltree)
+nodelabels(woltree$Nnode)
+dev.off()
+
+ star <- collapse.to.star(woltree, 164)
